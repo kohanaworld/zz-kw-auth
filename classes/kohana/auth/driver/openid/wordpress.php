372da170c9440b8d05d6c269977392c83f3229c4
biakaveron <biakaveron@gmail.com>
2011-11-11 23:30:39 +0400

    Fixed wrong usernames for WP OpenID driver

--- classes/kohana/auth/driver/openid/wordpress.php
+++ classes/kohana/auth/driver/openid/wordpress.php
@@ -4,4 +4,15 @@ abstract class Kohana_Auth_Driver_OpenID_Wordpress extends Auth_Driver_OpenID {
 
 	public $name = 'openid.wordpress';
 
+	protected function _get_user_data($user)
+	{
+		$result = parent::_get_user_data($user);
+
+		// wordpress login looks like http://username.wordpress.com
+		$username = trim(str_replace('http://', '', $result['service_name']), '/');
+		$result['service_name'] = current(explode('.', $username));
+
+		return $result;
+	}
+
 }
