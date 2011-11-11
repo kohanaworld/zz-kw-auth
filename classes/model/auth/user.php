ef6f2362afe912b6a45ae8ed77a77f2741dd8376
biakaveron <biakaveron@gmail.com>
2011-11-11 23:27:16 +0400

    Implements #2. Model_User::get_avatar() added.

--- classes/model/auth/user.php
+++ classes/model/auth/user.php
@@ -1,3 +1,42 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-class Model_Auth_User extends Model_Auth_Jelly_User {}
+class Model_Auth_User extends Model_Auth_Jelly_User {
+
+	public function get_avatar($size = NULL)
+	{
+		if ( ! $this->loaded() )
+		{
+			return FALSE;
+		}
+
+		$avatar = $this->auth_data->avatar;
+		if (empty($avatar) AND ! empty($this->email) )
+		{
+			$avatar = md5($this->email);
+		}
+
+		if (strpos($avatar, '://') == FALSE)
+		{
+			// its a Gravatar ID
+			$avatar = 'http://gravatar.com/avatar/' . $avatar;
+			$params = array();
+			if (empty($avatar))
+			{
+				// use default Gravatar
+				$params['f'] = 'y';
+			}
+
+			if ($size)
+			{
+				$params['s'] = intval($size);
+			}
+
+			if ( ! empty($params) )
+			{
+				$avatar .= http_build_query($params);
+			}
+		}
+
+		return $avatar;
+	}
+}
