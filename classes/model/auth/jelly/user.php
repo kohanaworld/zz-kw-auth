930230986e7d38273755711f86427813926bb4d3
biakaveron <biakaveron@gmail.com>
2011-11-11 23:29:29 +0400

    #3. Add email field to Model_User (primary email)

--- classes/model/auth/jelly/user.php
+++ classes/model/auth/jelly/user.php
@@ -8,6 +8,7 @@ abstract class Model_Auth_Jelly_User extends Jelly_Model {
 			->fields(array(
 				'id'         => Jelly::field('primary'),
 				'username'   => Jelly::field('string'),
+				'email'      => Jelly::field('email'),
 				'auth_data'  => Jelly::field('BelongsTo', array(
 					//'foreign'   => 'auth_data',
 					'column'     => 'auth_id',
